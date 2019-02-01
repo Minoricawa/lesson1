d0fdd494d697e918f3a64c9000c10d046a318498
Minoricawa <cruel-fancy.lie@live.jp>
2019-02-01 19:48:45 +0900

    初

---
+++ hensu.cs
@@ -0,0 +1,13 @@
+using System;
+
+class hensu {
+  static void Main (string[] args){
+    double h = 1.008;
+    double o = 16.00;
+    string m = "水";
+
+    Console.Write(m + "の分子量は" + h*2+o "である。");
+    Console.WriteLine("大体" + (int)h*2+o "。");
+  }
+
+}
