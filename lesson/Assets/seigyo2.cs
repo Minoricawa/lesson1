31c87cf3673797e6e8272178d71f17b2fff191d4
Minoricawa <cruel-fancy.lie@live.jp>
2019-02-09 13:17:39 +0900

    2/9

---
+++ lesson/Assets/seigyo2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seigyo2 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        int i = 0;
+        string e ="うん。";
+
+        while (i <= 100)
+        {
+            i = Random.Range(1, 100);
+
+            if (i <= 5)
+            {
+                Debug.Log(i+"  クリティカル＾＾");
+                break;
+            }
+            else if (i >= 96)
+            {
+                Debug.Log(i+"  ファンブル；；");
+                break;
+            }
+            else
+            {
+                Debug.LogFormat("{0}  {1}",i,e);
+                break;
+            }
+        }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
