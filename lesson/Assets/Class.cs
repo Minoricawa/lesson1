430b50485502fcc87f7bba56c44ad56ebeba53b6
Minoricawa <cruel-fancy.lie@live.jp>
2019-03-02 14:45:52 +0900

    クラスのやつ

---
+++ lesson/Assets/Class.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Color co = new Color();
+
+        co.name = "トマト";
+        co.color = "赤";
+
+        Debug.Log(co.name + "は" + co.color);
+
+
+        Abc a = new Abc();
+
+        Debug.Log(a.abc(co.name, co.color));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
+public class Color
+{
+    public string name;
+    public string color;
+
+}
+
+
+public class Abc : Color
+{
+    public string abc(string name, string color)
+    {
+        return name + " is " + color;
+    }
+}
